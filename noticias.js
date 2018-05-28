7a2ceee252618da82b94f005124f397adebd7991
kenzo98 <32308540+kenzo98@users.noreply.github.com>
2018-05-29 00:04:16 +0200

    Add files via upload

---
+++ noticias.js
@@ -0,0 +1,58 @@
+var cargado = 1;
+var indice = 1;
+var currentBanner = 2;
+var n = 1;
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() + $(window).height() + 10 >= $(document).height()) {
+		cargar2();
+    }
+});
+
+// $("#gear").click(function(){
+	// alert("")
+// })
+
+
+function cargar() {
+    cargar2();
+};
+
+function cargar2(){
+	if (cargado < 3) {
+        $.getJSON("https://rawgit.com/kenzo98/news/master/data/noticias" + indice + ".json", function (jsonObject) { addrows(jsonObject); }); 
+		cargado++;
+		indice = "2";
+    } else {
+        $('#mas').text('No hay más noticias');
+    }
+}
+
+function addrows(json) {
+    $.each(json, function (i, item) {
+        $("#grupo").append(
+        	'<div class="col-sm-6">'+
+				'<div class="thumbnail">'+
+				'<img src="'+item.img +'" alt="img">'+
+				'<h3>'+item.title+'</h3>'+
+				'<p>'+item.date+'</p>'+
+				'<p>'+item.sub+'</p>'+
+				'<button>Leer más</button>'+'</div>'+'</div>'
+			);
+	})
+};
+
+
+$(document).ready(function() {
+    $("#n"+n).click(function(){
+    	window.location.href=("Noticia" + n + ".html");
+		n++;
+    }); 
+});
+
+
+setInterval(function(){
+	$('#anuncio').attr('src','img/anuncio'+currentBanner+'.png');
+	currentBanner++;
+	if (currentBanner>3){currentBanner=1};
+	}, 3000);
