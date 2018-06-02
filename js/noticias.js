938a5394588bcfa9fb3ce8205e0b5c8c7a150668
kenzo98 <32308540+kenzo98@users.noreply.github.com>
2018-06-02 21:31:42 +0200

    Update noticias.js

--- js/noticias.js
+++ js/noticias.js
@@ -1,41 +1,56 @@
 var cargado = 1;
-var indice = 1;
 var currentBanner = 2;
-var n = 1;
+var scroll = true;
 
 $(window).scroll(function () {
     if ($(window).scrollTop() + $(window).height() + 10 >= $(document).height()) {
-		cargar();
+		cargar(1);
     }
 });
 
-function cargar(){
-	if (cargado <= 4) {
-        $.getJSON("https://rawgit.com/kenzo98/news/master/data/noticias" + cargado + ".json", function (jsonObject) {
-		addrows(jsonObject); }); 
-		cargado++;
-    } else {
-        $('#gear').text('No hay más noticias');
-    }
+$(document).ready(function() {
+	$("#switch").click(function(){
+		if(scroll==true){
+			scroll=false;
+			$('#switch').text('Activar Scroll');
+		}
+		else{
+			scroll=true;
+			$('#switch').text('Desactivar Scroll');
+		}
+	})
+	$("#gear").click(function(){
+		cargar(0);
+	})
+});
+
+function cargar(origen){
+	if(scroll==true||origen==0){
+		if (cargado <= 4){
+			$.getJSON("https://rawgit.com/kenzo98/news/master/data/noticias" + cargado + ".json", function (jsonObject) {
+			addrows(jsonObject); }); 
+			cargado++;
+		} else {
+			$('#gear').text('No hay más noticias');
+		}
+	}
 }
 
 function addrows(json) {
-    $.each(json, function (i, item) {
         $("#grupo").append('<div class="row">'+
-			   	'<div class="col-sm-6">'+
+				'<div class="col-sm-6">'+
 					'<div class="thumbnail">'+
-						'<img src="'+item[i].img +'" alt="img">'+
-						'<h3>'+item.title+'</h3>'+
-						'<p>'+item.date+'</p>'+
-						'<p>'+item.sub+'</p>'+'</div>'+'</div>'+
-			   	'<div class="col-sm-6">'+
+						'<img src="'+json[0].img +'" alt="img">'+
+						'<h3>'+json[0].title+'</h3>'+
+						'<p>'+json[0].date+'</p>'+
+						'<p>'+json[0].sub+'</p>'+'</div>'+'</div>'+
+				'<div class="col-sm-6">'+
 					'<div class="thumbnail">'+
-						'<img src="'+item[i].img +'" alt="img">'+
-						'<h3>'+item.title+'</h3>'+
-						'<p>'+item.date+'</p>'+
-						'<p>'+item.sub+'</p>'+'</div>'+'</div>'
-			);
-	});
+						'<img src="'+json[1].img +'" alt="img">'+
+						'<h3>'+json[1].title+'</h3>'+
+						'<p>'+json[1].date+'</p>'+
+						'<p>'+json[1].sub+'</p>'+'</div>'+'</div>'
+		);
 };
 
 setInterval(function(){
@@ -43,3 +58,4 @@ setInterval(function(){
 	currentBanner++;
 	if (currentBanner>3){currentBanner=1};
 	}, 3000);
+
